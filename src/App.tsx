652448d7b8aadf0734f1cafad0437e4cb434a670
Khalid <nwakala730@gmail.com>
2023-09-07 17:56:00 +0000

    chore: polish application styling

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>TaskFlow Pro</h1>
+      <p>Kanban workspace in progress.</p>
+    </div>
+  );
+}
