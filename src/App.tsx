b6edbb5c7a8e75f56cdef4c663bf763c9ea26edc
Khalid <nwakala730@gmail.com>
2023-09-08 18:18:00 +0000

    feat: polish application styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className="app">
-      <h1>TaskFlow Pro</h1>
-      <p>Kanban workspace in progress.</p>
+    <div className="app-shell">
+      <aside className="sidebar">
+        <h1 className="logo">TaskFlow</h1>
+        <p className="sidebar-note">Boards will appear here.</p>
+      </aside>
+      <main className="main">
+        <header className="header">Product Roadmap</header>
+        <section className="content">
+          <p>Build your first board.</p>
+        </section>
+      </main>
     </div>
   );
 }
