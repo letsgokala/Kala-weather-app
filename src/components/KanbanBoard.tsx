339df02b51c215c69e5ee45c42da99775f40bcbc
Khalid <nwakala730@gmail.com>
2023-09-10 16:52:00 +0000

    feat: iterate on board interactions

---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const KanbanBoard = () => {
+  return (
+    <div className="board">
+      <header className="board-header">Product Roadmap</header>
+      <div className="board-body">
+        <p>Kanban columns will go here.</p>
+      </div>
+    </div>
+  );
+};
