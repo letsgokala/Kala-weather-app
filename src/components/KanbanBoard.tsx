4a5ae7580b848a2de78614b8b52d31c1b86e6b6b
Khalid <nwakala730@gmail.com>
2023-09-12 08:26:00 +0000

    feat: iterate on board interactions

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Column } from './Column';
+
+const columns = [
+  { title: 'To Do', count: 0 },
+  { title: 'In Progress', count: 0 },
+  { title: 'Review', count: 0 },
+  { title: 'Done', count: 0 },
+];
 
 export const KanbanBoard = () => {
   return (
     <div className="board">
       <header className="board-header">Product Roadmap</header>
-      <div className="board-body">
-        <p>Kanban columns will go here.</p>
+      <div className="board-body columns">
+        {columns.map((column) => (
+          <Column key={column.title} title={column.title} count={column.count} />
+        ))}
       </div>
     </div>
   );
