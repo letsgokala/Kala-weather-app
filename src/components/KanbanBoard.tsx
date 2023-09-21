f1231ca7d30b8ff57c7065b49e749fc02697e0d2
Khalid <nwakala730@gmail.com>
2023-09-21 07:46:00 +0000

    feat: iterate on board interactions

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Column } from './Column';
-
-const columns = [
-  {
-    title: 'To Do',
-    tasks: [
-      { title: 'Define roadmap', description: 'Set milestones for the quarter.' },
-      { title: 'Review backlog' },
-    ],
-  },
-  {
-    title: 'In Progress',
-    tasks: [{ title: 'Build Kanban UI', description: 'Create columns and cards.' }],
-  },
-  { title: 'Review', tasks: [] },
-  { title: 'Done', tasks: [{ title: 'Project setup' }] },
-];
+import { useTaskStore } from '../store/useTaskStore';
 
 export const KanbanBoard = () => {
+  const { tasks } = useTaskStore();
+
+  const columns = [
+    { title: 'To Do', status: 'todo' },
+    { title: 'In Progress', status: 'in-progress' },
+    { title: 'Review', status: 'review' },
+    { title: 'Done', status: 'done' },
+  ];
+
   return (
     <div className="board">
       <header className="board-header">Product Roadmap</header>
       <div className="board-body columns">
         {columns.map((column) => (
-          <Column key={column.title} title={column.title} tasks={column.tasks} />
+          <Column
+            key={column.status}
+            title={column.title}
+            tasks={tasks.filter((task) => task.status === column.status)}
+          />
         ))}
       </div>
     </div>
