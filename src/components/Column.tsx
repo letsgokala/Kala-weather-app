8f9ca391d8fc92d856e944592ee87756232d7344
Khalid <nwakala730@gmail.com>
2023-09-22 18:01:00 +0000

    feat: improve column behavior

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { Droppable } from '@hello-pangea/dnd';
 import { Task } from '../types/task';
 import { TaskCard } from './TaskCard';
 
 interface ColumnProps {
+  id: string;
   title: string;
   tasks: Task[];
 }
 
-export const Column = ({ title, tasks }: ColumnProps) => {
+export const Column = ({ id, title, tasks }: ColumnProps) => {
   return (
     <div className="column">
       <div className="column-header">
         <span>{title}</span>
         <span className="column-count">{tasks.length}</span>
       </div>
-      <div className="column-body">
-        {tasks.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-      </div>
+      <Droppable droppableId={id}>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} className="column-body">
+            {tasks.map((task, index) => (
+              <TaskCard key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
