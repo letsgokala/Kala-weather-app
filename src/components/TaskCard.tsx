47b03a9f3cbd1e9b222260df467367d3e96c150d
Khalid <nwakala730@gmail.com>
2023-09-23 10:31:00 +0000

    feat: add drag and drop wiring

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Draggable } from '@hello-pangea/dnd';
 import { Task } from '../types/task';
 
 interface TaskCardProps {
   task: Task;
+  index: number;
 }
 
-export const TaskCard = ({ task }: TaskCardProps) => {
+export const TaskCard = ({ task, index }: TaskCardProps) => {
   return (
-    <div className="task-card">
-      <h4>{task.title}</h4>
-      <p>{task.description}</p>
-    </div>
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="task-card"
+        >
+          <h4>{task.title}</h4>
+          <p>{task.description}</p>
+        </div>
+      )}
+    </Draggable>
   );
 };
