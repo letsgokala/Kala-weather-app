eaf6709e990e6bcfa2fe89bad4bd27c93d9f648e
Khalid <nwakala730@gmail.com>
2023-09-28 07:27:00 +0000

    feat: refine workspace implementation

--- src/components/AddTaskModal.tsx
+++ src/components/AddTaskModal.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTaskStore } from '../store/useTaskStore';
 
 interface AddTaskModalProps {
   isOpen: boolean;
@@ -6,16 +7,57 @@ interface AddTaskModalProps {
 }
 
 export const AddTaskModal = ({ isOpen, onClose }: AddTaskModalProps) => {
+  const { addTask } = useTaskStore();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   if (!isOpen) {
     return null;
   }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!title.trim()) {
+      return;
+    }
+    addTask(title.trim(), description.trim());
+    setTitle('');
+    setDescription('');
+    onClose();
+  };
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         <h3>Create New Task</h3>
-        <p>Task form coming soon.</p>
-        <button onClick={onClose}>Close</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Title
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="What needs to be done?"
+            />
+          </label>
+          <label>
+            Description
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Add details"
+              rows={3}
+            />
+          </label>
+          <div className="modal-actions">
+            <button type="button" onClick={onClose}>
+              Cancel
+            </button>
+            <button type="submit" className="primary">
+              Create Task
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
