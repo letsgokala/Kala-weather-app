d28a27ca7ab911aec9150973b541b3ea56350ed8
Khalid <nwakala730@gmail.com>
2023-10-13 15:41:00 +0000

    refactor: normalize task store

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,13 +1,21 @@
 import { create } from 'zustand';
 import { Task, Priority } from '../types/task';
 
+interface ColumnState {
+  id: string;
+  title: string;
+  taskIds: string[];
+}
+
 interface TaskState {
-  tasks: Task[];
+  tasks: Record<string, Task>;
+  columns: Record<string, ColumnState>;
+  columnOrder: string[];
   addTask: (title: string, description: string, priority: Priority) => void;
 }
 
-const initialTasks: Task[] = [
-  {
+const initialTasks: Record<string, Task> = {
+  'task-1': {
     id: 'task-1',
     title: 'Design system audit',
     description: 'Review existing patterns and update tokens.',
@@ -16,13 +24,23 @@ const initialTasks: Task[] = [
     createdAt: Date.now(),
     tags: ['design']
   }
-];
+};
+
+const initialColumns: Record<string, ColumnState> = {
+  todo: { id: 'todo', title: 'To Do', taskIds: ['task-1'] },
+  'in-progress': { id: 'in-progress', title: 'In Progress', taskIds: [] },
+  review: { id: 'review', title: 'Review', taskIds: [] },
+  done: { id: 'done', title: 'Done', taskIds: [] }
+};
 
 export const useTaskStore = create<TaskState>((set) => ({
   tasks: initialTasks,
+  columns: initialColumns,
+  columnOrder: ['todo', 'in-progress', 'review', 'done'],
   addTask: (title, description, priority) => {
+    const id = `task-${Date.now()}`;
     const newTask: Task = {
-      id: `task-${Date.now()}`,
+      id,
       title,
       description,
       status: 'todo',
@@ -30,6 +48,15 @@ export const useTaskStore = create<TaskState>((set) => ({
       createdAt: Date.now(),
       tags: []
     };
-    set((state) => ({ tasks: [...state.tasks, newTask] }));
+    set((state) => ({
+      tasks: { ...state.tasks, [id]: newTask },
+      columns: {
+        ...state.columns,
+        todo: {
+          ...state.columns.todo,
+          taskIds: [...state.columns.todo.taskIds, id]
+        }
+      }
+    }));
   }
 }));
