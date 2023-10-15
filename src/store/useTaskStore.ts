6acd80152927226a27f66602a6610b2e12e442fb
Khalid <nwakala730@gmail.com>
2023-10-15 16:29:00 +0000

    feat: persist board state

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { v4 as uuidv4 } from 'uuid';
 import { Task, Priority } from '../types/task';
 
 interface ColumnState {
@@ -41,68 +43,75 @@ const initialColumns: Record<string, ColumnState> = {
   done: { id: 'done', title: 'Done', taskIds: [] }
 };
 
-export const useTaskStore = create<TaskState>((set) => ({
-  tasks: initialTasks,
-  columns: initialColumns,
-  columnOrder: ['todo', 'in-progress', 'review', 'done'],
-  addTask: (title, description, priority) => {
-    const id = `task-${Date.now()}`;
-    const newTask: Task = {
-      id,
-      title,
-      description,
-      status: 'todo',
-      priority,
-      createdAt: Date.now(),
-      tags: []
-    };
-    set((state) => ({
-      tasks: { ...state.tasks, [id]: newTask },
-      columns: {
-        ...state.columns,
-        todo: {
-          ...state.columns.todo,
-          taskIds: [...state.columns.todo.taskIds, id]
-        }
+export const useTaskStore = create<TaskState>()(
+  persist(
+    (set) => ({
+      tasks: initialTasks,
+      columns: initialColumns,
+      columnOrder: ['todo', 'in-progress', 'review', 'done'],
+      addTask: (title, description, priority) => {
+        const id = uuidv4();
+        const newTask: Task = {
+          id,
+          title,
+          description,
+          status: 'todo',
+          priority,
+          createdAt: Date.now(),
+          tags: []
+        };
+        set((state) => ({
+          tasks: { ...state.tasks, [id]: newTask },
+          columns: {
+            ...state.columns,
+            todo: {
+              ...state.columns.todo,
+              taskIds: [...state.columns.todo.taskIds, id]
+            }
+          }
+        }));
+      },
+      moveTask: (sourceColumnId, destinationColumnId, sourceIndex, destinationIndex, taskId) => {
+        set((state) => {
+          const sourceColumn = state.columns[sourceColumnId];
+          const destinationColumn = state.columns[destinationColumnId];
+          const sourceTaskIds = Array.from(sourceColumn.taskIds);
+          const destinationTaskIds = Array.from(destinationColumn.taskIds);
+
+          sourceTaskIds.splice(sourceIndex, 1);
+          destinationTaskIds.splice(destinationIndex, 0, taskId);
+
+          return {
+            tasks: {
+              ...state.tasks,
+              [taskId]: { ...state.tasks[taskId], status: destinationColumnId }
+            },
+            columns: {
+              ...state.columns,
+              [sourceColumnId]: { ...sourceColumn, taskIds: sourceTaskIds },
+              [destinationColumnId]: { ...destinationColumn, taskIds: destinationTaskIds }
+            }
+          };
+        });
+      },
+      reorderColumn: (columnId, startIndex, endIndex) => {
+        set((state) => {
+          const column = state.columns[columnId];
+          const newTaskIds = Array.from(column.taskIds);
+          const [removed] = newTaskIds.splice(startIndex, 1);
+          newTaskIds.splice(endIndex, 0, removed);
+
+          return {
+            columns: {
+              ...state.columns,
+              [columnId]: { ...column, taskIds: newTaskIds }
+            }
+          };
+        });
       }
-    }));
-  },
-  moveTask: (sourceColumnId, destinationColumnId, sourceIndex, destinationIndex, taskId) => {
-    set((state) => {
-      const sourceColumn = state.columns[sourceColumnId];
-      const destinationColumn = state.columns[destinationColumnId];
-      const sourceTaskIds = Array.from(sourceColumn.taskIds);
-      const destinationTaskIds = Array.from(destinationColumn.taskIds);
-
-      sourceTaskIds.splice(sourceIndex, 1);
-      destinationTaskIds.splice(destinationIndex, 0, taskId);
-
-      return {
-        tasks: {
-          ...state.tasks,
-          [taskId]: { ...state.tasks[taskId], status: destinationColumnId }
-        },
-        columns: {
-          ...state.columns,
-          [sourceColumnId]: { ...sourceColumn, taskIds: sourceTaskIds },
-          [destinationColumnId]: { ...destinationColumn, taskIds: destinationTaskIds }
-        }
-      };
-    });
-  },
-  reorderColumn: (columnId, startIndex, endIndex) => {
-    set((state) => {
-      const column = state.columns[columnId];
-      const newTaskIds = Array.from(column.taskIds);
-      const [removed] = newTaskIds.splice(startIndex, 1);
-      newTaskIds.splice(endIndex, 0, removed);
-
-      return {
-        columns: {
-          ...state.columns,
-          [columnId]: { ...column, taskIds: newTaskIds }
-        }
-      };
-    });
-  }
-}));
+    }),
+    {
+      name: 'taskflow-storage'
+    }
+  )
+);
