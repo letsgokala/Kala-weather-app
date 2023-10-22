ad09cbf3c45ea34c50a13c49c3ab5931b11b7bcd
Khalid <nwakala730@gmail.com>
2023-10-22 16:56:00 +0000

    feat: polish application styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { KanbanBoard } from './components/KanbanBoard';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div className="app-shell">
-      <aside className="sidebar">
-        <h1 className="logo">TaskFlow</h1>
-        <p className="sidebar-note">Boards will appear here.</p>
-      </aside>
-      <main className="main">
-        <KanbanBoard />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app-shell">
+        <aside className="sidebar">
+          <h1 className="logo">TaskFlow</h1>
+          <p className="sidebar-note">Boards will appear here.</p>
+        </aside>
+        <main className="main">
+          <KanbanBoard />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
