65ccf24a594e5aa9ce02c8e681cfc1900f72e9bc
Khalid <nwakala730@gmail.com>
2023-10-24 06:00:00 +0000

    feat: connect assistant to api

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useMutation } from '@tanstack/react-query';
 import { ChatInput } from './ChatInput';
 import { MessageBubble } from './MessageBubble';
-import { Message } from '../types/chat';
+import { useChatStore } from '../store/useChatStore';
+import { generateChatResponse } from '../services/gemini';
 
 export const ChatWindow = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const { sessions, currentSessionId, addMessage, createSession } = useChatStore();
+  const currentSession = sessions.find((session) => session.id === currentSessionId);
+
+  const mutation = useMutation({
+    mutationFn: async (messages: any[]) => generateChatResponse(messages),
+    onSuccess: (response) => {
+      if (!currentSessionId) {
+        return;
+      }
+      addMessage(currentSessionId, {
+        id: crypto.randomUUID(),
+        role: 'assistant',
+        content: response,
+        timestamp: Date.now(),
+      });
+    },
+  });
 
   const handleSend = (content: string) => {
-    setMessages((prev) => [
-      ...prev,
-      {
-        id: crypto.randomUUID(),
-        role: 'user',
-        content,
-        timestamp: Date.now(),
-      },
-    ]);
+    const sessionId = currentSessionId ?? createSession();
+    const userMessage = {
+      id: crypto.randomUUID(),
+      role: 'user' as const,
+      content,
+      timestamp: Date.now(),
+    };
+
+    addMessage(sessionId, userMessage);
+    mutation.mutate([...(currentSession?.messages ?? []), userMessage]);
   };
 
   return (
     <div className="chat-window">
       <header className="chat-header">Assistant</header>
       <div className="chat-body">
-        {messages.length === 0 ? (
+        {!currentSession || currentSession.messages.length === 0 ? (
           <p>Start a conversation.</p>
         ) : (
-          messages.map((message) => (
+          currentSession.messages.map((message) => (
             <MessageBubble key={message.id} message={message} />
           ))
         )}
