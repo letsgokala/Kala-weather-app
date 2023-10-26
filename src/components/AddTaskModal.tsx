cf2964d63c14a6a44605ca462b7eadc6fd52a854
Khalid <nwakala730@gmail.com>
2023-10-26 16:38:00 +0000

    ui: improve column behavior

--- src/components/AddTaskModal.tsx
+++ src/components/AddTaskModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useTaskStore } from '../store/useTaskStore';
 import { Priority } from '../types/task';
+import { X } from 'lucide-react';
+import { motion, AnimatePresence } from 'motion/react';
 
 interface AddTaskModalProps {
   isOpen: boolean;
@@ -13,70 +15,99 @@ export const AddTaskModal = ({ isOpen, onClose }: AddTaskModalProps) => {
   const [description, setDescription] = useState('');
   const [priority, setPriority] = useState<Priority>('medium');
 
-  if (!isOpen) {
-    return null;
-  }
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (!title.trim()) {
-      return;
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTask('todo', title, description, priority);
+      setTitle('');
+      setDescription('');
+      setPriority('medium');
+      onClose();
     }
-    addTask(title.trim(), description.trim(), priority);
-    setTitle('');
-    setDescription('');
-    setPriority('medium');
-    onClose();
   };
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <h3>Create New Task</h3>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Title
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="What needs to be done?"
-            />
-          </label>
-          <label>
-            Description
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Add details"
-              rows={3}
-            />
-          </label>
-          <label>
-            Priority
-            <div className="priority">
-              {(['low', 'medium', 'high'] as Priority[]).map((value) => (
-                <button
-                  type="button"
-                  key={value}
-                  onClick={() => setPriority(value)}
-                  className={priority === value ? 'active' : ''}
-                >
-                  {value}
-                </button>
-              ))}
+    <AnimatePresence>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={onClose}
+            className="absolute inset-0 bg-black/60 backdrop-blur-sm"
+          />
+          
+          <motion.div
+            initial={{ opacity: 0, scale: 0.95, y: 20 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.95, y: 20 }}
+            className="relative w-full max-w-md bg-brand-surface border border-brand-border rounded-2xl shadow-2xl p-6"
+          >
+            <div className="flex items-center justify-between mb-6">
+              <h3 className="text-xl font-bold tracking-tight">Create New Task</h3>
+              <button onClick={onClose} className="p-2 hover:bg-white/5 rounded-lg text-brand-muted">
+                <X size={20} />
+              </button>
             </div>
-          </label>
-          <div className="modal-actions">
-            <button type="button" onClick={onClose}>
-              Cancel
-            </button>
-            <button type="submit" className="primary">
-              Create Task
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="space-y-1.5">
+                <label className="text-xs font-semibold uppercase tracking-wider opacity-40">Title</label>
+                <input
+                  autoFocus
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="What needs to be done?"
+                  className="w-full bg-white/5 border border-brand-border rounded-xl px-4 py-2.5 text-sm focus:outline-none focus:border-white/20 transition-all"
+                />
+              </div>
+
+              <div className="space-y-1.5">
+                <label className="text-xs font-semibold uppercase tracking-wider opacity-40">Description</label>
+                <textarea
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  placeholder="Add some details..."
+                  rows={3}
+                  className="w-full bg-white/5 border border-brand-border rounded-xl px-4 py-2.5 text-sm focus:outline-none focus:border-white/20 transition-all resize-none"
+                />
+              </div>
+
+              <div className="space-y-1.5">
+                <label className="text-xs font-semibold uppercase tracking-wider opacity-40">Priority</label>
+                <div className="flex gap-2">
+                  {(['low', 'medium', 'high'] as Priority[]).map((p) => (
+                    <button
+                      key={p}
+                      type="button"
+                      onClick={() => setPriority(p)}
+                      className={`flex-1 py-2 rounded-xl text-xs font-bold uppercase tracking-wider border transition-all ${
+                        priority === p 
+                          ? 'bg-white text-black border-white' 
+                          : 'bg-white/5 border-brand-border text-brand-muted hover:bg-white/10'
+                      }`}
+                    >
+                      {p}
+                    </button>
+                  ))}
+                </div>
+              </div>
+
+              <div className="pt-4">
+                <button
+                  type="submit"
+                  disabled={!title.trim()}
+                  className="w-full py-3 bg-white text-black rounded-xl font-bold hover:bg-white/90 transition-all disabled:opacity-50 disabled:cursor-not-allowed shadow-lg shadow-white/5"
+                >
+                  Create Task
+                </button>
+              </div>
+            </form>
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
   );
 };
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Droppable } from '@hello-pangea/dnd';
-import { Task } from '../types/task';
 import { TaskCard } from './TaskCard';
+import { Task } from '../types/task';
 import { Plus, MoreHorizontal } from 'lucide-react';
+import { cn } from '../lib/utils';
 
 interface ColumnProps {
   id: string;
@@ -12,24 +13,36 @@ interface ColumnProps {
 
 export const Column = ({ id, title, tasks }: ColumnProps) => {
   return (
-    <div className="column">
-      <div className="column-header">
-        <div className="column-title">
-          <span>{title}</span>
-          <span className="column-count">{tasks.length}</span>
+    <div className="flex flex-col w-80 shrink-0 h-full">
+      <div className="flex items-center justify-between px-2 mb-4">
+        <div className="flex items-center gap-2">
+          <h3 className="text-sm font-semibold uppercase tracking-widest opacity-60">
+            {title}
+          </h3>
+          <span className="px-2 py-0.5 bg-white/5 border border-white/10 rounded-full text-[10px] font-bold">
+            {tasks.length}
+          </span>
         </div>
-        <div className="column-actions">
-          <button className="icon-button">
-            <Plus size={14} />
+        <div className="flex items-center gap-1">
+          <button className="p-1.5 hover:bg-white/5 rounded-lg transition-all text-brand-muted hover:text-white">
+            <Plus size={16} />
           </button>
-          <button className="icon-button">
-            <MoreHorizontal size={14} />
+          <button className="p-1.5 hover:bg-white/5 rounded-lg transition-all text-brand-muted hover:text-white">
+            <MoreHorizontal size={16} />
           </button>
         </div>
       </div>
+
       <Droppable droppableId={id}>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps} className="column-body">
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className={cn(
+              "flex-1 px-2 py-1 rounded-2xl transition-all min-h-[150px]",
+              snapshot.isDraggingOver ? "bg-white/[0.02]" : "bg-transparent"
+            )}
+          >
             {tasks.map((task, index) => (
               <TaskCard key={task.id} task={task} index={index} />
             ))}
