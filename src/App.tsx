e46d1049556b50c1356aadc90e8921d4ab373691
Khalid <nwakala730@gmail.com>
2023-10-26 06:04:00 +0000

    ui: configure project metadata and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { KanbanBoard } from './components/KanbanBoard';
@@ -7,15 +12,11 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="app-shell">
-        <aside className="sidebar">
-          <h1 className="logo">TaskFlow</h1>
-          <p className="sidebar-note">Boards will appear here.</p>
-        </aside>
-        <main className="main">
-          <KanbanBoard />
-        </main>
+      <div className="flex h-screen w-full overflow-hidden font-sans">
+        <KanbanBoard />
       </div>
     </QueryClientProvider>
   );
 }
+
+
