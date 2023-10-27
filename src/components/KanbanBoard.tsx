e265d6b303504ef7802e45011409218752ceb05f
Khalid <nwakala730@gmail.com>
2023-10-27 07:43:00 +0000

    ui: iterate on board interactions

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,79 +1,163 @@
 import React, { useState } from 'react';
 import { DragDropContext, DropResult } from '@hello-pangea/dnd';
-import { Column } from './Column';
 import { useTaskStore } from '../store/useTaskStore';
+import { Column } from './Column';
 import { AddTaskModal } from './AddTaskModal';
+import { 
+  Search, 
+  Filter, 
+  LayoutGrid, 
+  List, 
+  Plus, 
+  Bell, 
+  Settings,
+  Menu,
+  ChevronDown
+} from 'lucide-react';
+import { motion } from 'motion/react';
 
 export const KanbanBoard = () => {
   const { tasks, columns, columnOrder, moveTask, reorderColumn } = useTaskStore();
+  const [searchQuery, setSearchQuery] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [query, setQuery] = useState('');
 
   const onDragEnd = (result: DropResult) => {
     const { destination, source, draggableId } = result;
 
-    if (!destination) {
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
       return;
     }
 
-    if (destination.droppableId === source.droppableId) {
+    if (source.droppableId === destination.droppableId) {
       reorderColumn(source.droppableId, source.index, destination.index);
-      return;
+    } else {
+      moveTask(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId
+      );
     }
-
-    moveTask(
-      source.droppableId,
-      destination.droppableId,
-      source.index,
-      destination.index,
-      draggableId
-    );
   };
 
   return (
-    <div className="board">
-      <header className="board-header">
-        <div>
-          <div className="board-title">Product Roadmap</div>
-          <p className="board-subtitle">Track priorities across the team.</p>
+    <div className="flex-1 flex flex-col h-screen bg-brand-bg overflow-hidden">
+      {/* Top Navbar */}
+      <header className="h-16 border-b border-brand-border flex items-center px-8 justify-between bg-brand-bg/50 backdrop-blur-md z-20">
+        <div className="flex items-center gap-8">
+          <div className="flex items-center gap-2">
+            <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
+              <div className="w-4 h-4 bg-black rounded-sm" />
+            </div>
+            <h1 className="text-lg font-bold tracking-tight">TaskFlow</h1>
+          </div>
+          
+          <nav className="hidden md:flex items-center gap-1">
+            {['Dashboard', 'My Tasks', 'Projects', 'Team'].map((item) => (
+              <button 
+                key={item}
+                className="px-4 py-2 text-sm font-medium text-brand-muted hover:text-white transition-all rounded-lg hover:bg-white/5"
+              >
+                {item}
+              </button>
+            ))}
+          </nav>
         </div>
-        <div className="board-actions">
-          <input
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Search tasks"
-            className="search"
-          />
-          <button className="primary" onClick={() => setIsModalOpen(true)}>
-            Add Task
+
+        <div className="flex items-center gap-4">
+          <div className="relative hidden sm:block">
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-brand-muted" size={16} />
+            <input 
+              type="text"
+              placeholder="Search tasks..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="bg-white/5 border border-brand-border rounded-xl pl-10 pr-4 py-2 text-sm focus:outline-none focus:border-white/20 transition-all w-64"
+            />
+          </div>
+          <button className="p-2 hover:bg-white/5 rounded-xl text-brand-muted hover:text-white transition-all">
+            <Bell size={20} />
           </button>
+          <button className="p-2 hover:bg-white/5 rounded-xl text-brand-muted hover:text-white transition-all">
+            <Settings size={20} />
+          </button>
+          <div className="w-8 h-8 rounded-full bg-gradient-to-br from-indigo-500 to-purple-500 border border-white/10" />
         </div>
       </header>
-      <div className="board-body columns">
-        <DragDropContext onDragEnd={onDragEnd}>
-          {columnOrder.map((columnId) => {
-            const column = columns[columnId];
-            const columnTasks = column.taskIds
-              .map((taskId) => tasks[taskId])
-              .filter((task) =>
-                `${task.title} ${task.description}`
-                  .toLowerCase()
-                  .includes(query.toLowerCase()),
-              );
 
-            return (
-              <Column
-                key={column.id}
-                id={column.id}
-                title={column.title}
-                tasks={columnTasks}
-              />
-            );
-          })}
-          <div className="column add-column">+ Add Section</div>
-        </DragDropContext>
+      {/* Sub-header */}
+      <div className="px-8 py-6 border-b border-brand-border flex flex-col sm:flex-row sm:items-center justify-between gap-4 bg-brand-bg">
+        <div>
+          <div className="flex items-center gap-3 mb-1">
+            <h2 className="text-2xl font-bold tracking-tight">Product Roadmap</h2>
+            <button className="p-1 hover:bg-white/5 rounded-md text-brand-muted">
+              <ChevronDown size={16} />
+            </button>
+          </div>
+          <p className="text-sm text-brand-muted">Manage and track your product development lifecycle.</p>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <div className="flex items-center bg-white/5 border border-brand-border rounded-xl p-1">
+            <button className="p-1.5 bg-white/10 text-white rounded-lg shadow-sm">
+              <LayoutGrid size={18} />
+            </button>
+            <button className="p-1.5 text-brand-muted hover:text-white rounded-lg transition-all">
+              <List size={18} />
+            </button>
+          </div>
+          <button 
+            onClick={() => setIsModalOpen(true)}
+            className="flex items-center gap-2 px-4 py-2 bg-white text-black rounded-xl text-sm font-bold hover:bg-white/90 transition-all shadow-lg shadow-white/5"
+          >
+            <Plus size={18} />
+            <span>Add Task</span>
+          </button>
+        </div>
       </div>
+
+      {/* Kanban Board */}
+      <main className="flex-1 overflow-x-auto overflow-y-hidden p-8 bg-[#080808]">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="flex gap-8 h-full min-w-max">
+            {columnOrder.map((columnId) => {
+              const column = columns[columnId];
+              const columnTasks = column.taskIds
+                .map((taskId) => tasks[taskId])
+                .filter(task => 
+                  task.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                  task.description.toLowerCase().includes(searchQuery.toLowerCase())
+                );
+
+              return (
+                <Column 
+                  key={column.id} 
+                  id={column.id} 
+                  title={column.title} 
+                  tasks={columnTasks} 
+                />
+              );
+            })}
+            
+            {/* Add Column Placeholder */}
+            <div className="w-80 shrink-0 h-full">
+              <button className="w-full h-12 border border-dashed border-brand-border rounded-2xl flex items-center justify-center gap-2 text-brand-muted hover:text-white hover:border-white/20 transition-all group">
+                <Plus size={18} className="group-hover:scale-110 transition-transform" />
+                <span className="text-sm font-medium">Add Section</span>
+              </button>
+            </div>
+          </div>
+        </DragDropContext>
+      </main>
+
       <AddTaskModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
+
   );
 };
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Draggable } from '@hello-pangea/dnd';
 import { Task, Priority } from '../types/task';
+import { cn } from '../lib/utils';
+import { Clock, Tag, MoreHorizontal } from 'lucide-react';
 import { format } from 'date-fns';
 
 interface TaskCardProps {
@@ -9,34 +11,56 @@ interface TaskCardProps {
 }
 
 const priorityColors: Record<Priority, string> = {
-  low: 'priority low',
-  medium: 'priority medium',
-  high: 'priority high'
+  low: 'bg-emerald-500/10 text-emerald-500 border-emerald-500/20',
+  medium: 'bg-amber-500/10 text-amber-500 border-amber-500/20',
+  high: 'bg-rose-500/10 text-rose-500 border-rose-500/20'
 };
 
 export const TaskCard = ({ task, index }: TaskCardProps) => {
   return (
     <Draggable draggableId={task.id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          className="task-card"
+          className={cn(
+            "group bg-brand-surface border border-brand-border rounded-xl p-4 mb-3 transition-all hover:border-white/20 hover:shadow-lg",
+            snapshot.isDragging ? "shadow-2xl border-white/30 rotate-2" : ""
+          )}
         >
-          <div className="task-meta">
-            <span className={priorityColors[task.priority]}>{task.priority}</span>
-            <span className="task-date">{format(task.createdAt, 'MMM d')}</span>
+          <div className="flex items-start justify-between mb-2">
+            <span className={cn(
+              "px-2 py-0.5 rounded-full text-[10px] font-semibold uppercase tracking-wider border",
+              priorityColors[task.priority]
+            )}>
+              {task.priority}
+            </span>
+            <button className="opacity-0 group-hover:opacity-100 p-1 hover:bg-white/5 rounded-md transition-all">
+              <MoreHorizontal size={14} className="text-brand-muted" />
+            </button>
           </div>
-          <h4>{task.title}</h4>
-          <p>{task.description}</p>
-          {task.tags.length > 0 && (
-            <div className="task-tags">
-              {task.tags.map((tag) => (
-                <span key={tag}>{tag}</span>
+          
+          <h4 className="text-sm font-medium mb-1 line-clamp-2">{task.title}</h4>
+          <p className="text-xs text-brand-muted mb-4 line-clamp-3 leading-relaxed">
+            {task.description}
+          </p>
+          
+          <div className="flex items-center justify-between pt-3 border-t border-brand-border/50">
+            <div className="flex items-center gap-1.5 text-[10px] text-brand-muted">
+              <Clock size={12} />
+              <span>{format(task.createdAt, 'MMM d')}</span>
+            </div>
+            
+            <div className="flex -space-x-1.5">
+              {[1, 2].map((i) => (
+                <div 
+                  key={i}
+                  className="w-5 h-5 rounded-full border border-brand-surface bg-gradient-to-br from-indigo-500 to-purple-500"
+                />
               ))}
             </div>
-          )}
+          </div>
         </div>
       )}
     </Draggable>
