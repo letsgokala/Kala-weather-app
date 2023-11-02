41a26c105d3e0d8bbbeb6d662fcfc1d8756d80dd
Khalid <nwakala730@gmail.com>
2023-11-02 13:44:00 +0000

    feat: improve column behavior

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Droppable } from '@hello-pangea/dnd';
 import { TaskCard } from './TaskCard';
 import { Task } from '../types/task';
-import { Plus, MoreHorizontal } from 'lucide-react';
+import { Plus, MoreHorizontal, PencilLine, Trash2 } from 'lucide-react';
 import { cn } from '../lib/utils';
 
 interface ColumnProps {
   id: string;
   title: string;
   tasks: Task[];
+  onAddTask?: (columnId: string) => void;
+  onRename?: (columnId: string) => void;
+  onDelete?: (columnId: string) => void;
+  canDelete?: boolean;
 }
 
-export const Column = ({ id, title, tasks }: ColumnProps) => {
+export const Column = ({
+  id,
+  title,
+  tasks,
+  onAddTask,
+  onRename,
+  onDelete,
+  canDelete = true
+}: ColumnProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [isMenuOpen]);
+
   return (
     <div className="flex flex-col w-80 shrink-0 h-full">
       <div className="flex items-center justify-between px-2 mb-4">
@@ -23,13 +51,52 @@ export const Column = ({ id, title, tasks }: ColumnProps) => {
             {tasks.length}
           </span>
         </div>
-        <div className="flex items-center gap-1">
-          <button className="p-1.5 hover:bg-white/5 rounded-lg transition-all text-brand-muted hover:text-white">
+        <div className="flex items-center gap-1 relative" ref={menuRef}>
+          <button
+            onClick={() => onAddTask?.(id)}
+            className="p-1.5 hover:bg-white/5 rounded-lg transition-all text-brand-muted hover:text-white"
+          >
             <Plus size={16} />
           </button>
-          <button className="p-1.5 hover:bg-white/5 rounded-lg transition-all text-brand-muted hover:text-white">
+          <button
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+            className="p-1.5 hover:bg-white/5 rounded-lg transition-all text-brand-muted hover:text-white"
+          >
             <MoreHorizontal size={16} />
           </button>
+
+          {isMenuOpen && (
+            <div className="absolute right-0 top-9 w-40 bg-brand-surface border border-brand-border rounded-xl shadow-xl p-1 z-20">
+              <button
+                onClick={() => {
+                  setIsMenuOpen(false);
+                  onRename?.(id);
+                }}
+                className="w-full flex items-center gap-2 px-3 py-2 rounded-lg text-xs text-brand-muted hover:text-white hover:bg-white/5"
+              >
+                <PencilLine size={14} />
+                Rename
+              </button>
+              <button
+                onClick={() => {
+                  setIsMenuOpen(false);
+                  if (canDelete) {
+                    onDelete?.(id);
+                  }
+                }}
+                disabled={!canDelete}
+                className={cn(
+                  "w-full flex items-center gap-2 px-3 py-2 rounded-lg text-xs",
+                  canDelete
+                    ? "text-red-400 hover:bg-red-500/10"
+                    : "text-brand-muted cursor-not-allowed"
+                )}
+              >
+                <Trash2 size={14} />
+                Delete
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
