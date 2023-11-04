a16e0ae7c4f4a23cf7a558198b9cb83df8ccf755
Khalid <nwakala730@gmail.com>
2023-11-04 16:16:00 +0000

    feat: enable column management

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,17 +1,18 @@
 export type Priority = 'low' | 'medium' | 'high';
+export type ColumnId = string;
 
 export interface Task {
   id: string;
   title: string;
   description: string;
-  status: 'todo' | 'in-progress' | 'review' | 'done';
+  status: ColumnId;
   priority: Priority;
   createdAt: number;
   tags: string[];
 }
 
 export interface Column {
-  id: string;
+  id: ColumnId;
   title: string;
   taskIds: string[];
 }
